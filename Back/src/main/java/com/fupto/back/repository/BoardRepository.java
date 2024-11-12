fe0f85c81d2860b90e1e2446e9d1dde104e583b1
109an94 <109an94@gmail.com>
2024-11-12 18:26:24 +0900

    al member detail 바ì ì 제작

--- Back/src/main/java/com/fupto/back/repository/BoardRepository.java
+++ Back/src/main/java/com/fupto/back/repository/BoardRepository.java
@@ -2,8 +2,14 @@ package com.fupto.back.repository;
 
 import com.fupto.back.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("select b from Board b where b.regMember.id = :id")
+    List<Board> findByRegMemberId(Long id);
 }
