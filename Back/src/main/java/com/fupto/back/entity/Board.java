0d4a42547028094fdc01e48d24ee2f183bef80b3
MIN <phm9571@naver.com>
2024-11-13 09:52:07 +0900

    merge

--- Back/src/main/java/com/fupto/back/entity/Board.java
+++ Back/src/main/java/com/fupto/back/entity/Board.java
@@ -1,6 +1,5 @@
 package com.fupto.back.entity;
 
-<<<<<<< HEAD
 import com.fupto.back.admin.board.dto.BoardRequestsDto;
 import jakarta.persistence.*;
 import lombok.*;
@@ -13,18 +12,6 @@ import java.time.Instant;
 @Data
 @Entity
 @NoArgsConstructor
-=======
-import jakarta.persistence.*;
-import lombok.*;
-
-@Getter
-@Setter
-@ToString
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
->>>>>>> 63ddb87f6ed60decd1963344edbc09225c17c3b1
 @Table(name = "board")
 public class Board {
     @Id
@@ -37,7 +24,6 @@ public class Board {
 
     @Column(name = "contents", nullable = false, length = 1000)
     private String contents;
-<<<<<<< HEAD
 
     @Column(name = "author", nullable = false, length = 200)
     private String author;
@@ -86,17 +72,3 @@ public class Board {
     }
 }
 
-=======
-
-    @Column(name = "author", nullable = false, length = 200)
-    private String author;
-
-    @Column(name = "password", nullable = false, length = 200)
-    private String password;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "reg_member_id", nullable = false)
-    private Member regMember;
-
-}
->>>>>>> 63ddb87f6ed60decd1963344edbc09225c17c3b1
